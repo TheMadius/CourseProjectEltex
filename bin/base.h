b07bd71764fbb6326b37e2fe0aede322534b8694
SalkovN17 <nikita_8.kz@mail.ru>
2021-09-15 09:20:01 +0700

    В структурах Ont_connection и Ont_input_info исправил массивы, на указатели, так как теперь константы задаются не с помощью define, а с помощью const.

--- bin/base.h
+++ bin/base.h
@@ -1,13 +1,15 @@
+ #include <time.h>
+
  /* Число символов для обозначения серийного номера ONT в текстовом формате. */
-const ONT_SERIAL_NUM_SIZE  16   
+int const   ONT_SERIAL_NUM_SIZE = 16;   
  /* Число символов для обозначения наименования ONT в текстовом формате. */     
-const ONT_NAME_SIZE  18  
+int const   ONT_EQ_ID_SIZE = 18;  
 /* Число символов для обозначения версии прошивки ONT в текстовом формате. */            
-const ONT_FW_VERSION_SIZE 32   
+int const   ONT_FW_VERSION_SIZE = 32;  
  /* Число записей для одного ONT соединения. */       
-const NUM_OF_RECORDS 10       
+int const   NUM_OF_RECORDS = 10;       
  /* Число ONT соединений. */
-const NUM_OF_ONT_CONNECTIONS 2048    
+int const   NUM_OF_ONT_CONNECTIONS = 2048;    
 
 /* Статусы ONT соединения. */
 typedef enum Ont_status
@@ -25,11 +27,11 @@ typedef enum Ont_status
 typedef struct Ont_connection
 {
     /* Серийный номер ONT в текстовом формате. */
-    char serial[ONT_SERIAL_NUM_SIZE]; 
+    char* serial; 
     /* Наименование ONT в текстовом формате. */       
-    char eq_id[ONT_NAME_SIZE];   
+    char* eq_id;   
     /* Версия прошивки ONT в текстовом формате. */    
-    char fw_version[ONT_FW_VERSION_SIZE];    
+    char* fw_version;    
     /* Время активации. День, год, час, минута, секунда. */
     time_t link_up;   
     /* Время деактивации. День, год, час, минута, секунда. */      
@@ -39,6 +41,7 @@ typedef struct Ont_connection
 
 } Ont_connection;
 
+/* Входная информация об ONT соединении. */
 typedef struct Ont_input_info
 {
     /* Номер порта */
@@ -46,22 +49,23 @@ typedef struct Ont_input_info
     /* Номер устройства  */
     int num_ont;
     /* Серийный номер ONT в текстовом формате. */
-    char serial[ONT_SERIAL_NUM_SIZE]; 
+    char* serial; 
     /* Наименование ONT в текстовом формате. */       
-    char eq_id[ONT_NAME_SIZE];   
+    char* eq_id;   
     /* Версия прошивки ONT в текстовом формате. */    
-    char fw_version[ONT_FW_VERSION_SIZE];    
+    char* fw_version;    
     /* Время активации. День, год, час, минута, секунда. */
     time_t link_up;   
     /* Время деактивации. День, год, час, минута, секунда. */      
     time_t link_down;   
     /* Статус ONT. Возможные статусы: Activation, Working, CfgFail, Block. */    
     enum Ont_status status; 
-} Ont_input_info
+} Ont_input_info;
 
 /* Список записей на массиве.  */
 typedef struct Ont_records
 {
     int cur_index_of_event; 
-    struct Ont_connection records[NUM_OF_RECORDS];
+    struct Ont_connection* records;
 } Ont_records;
+
