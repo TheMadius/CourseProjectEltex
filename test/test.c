e5efdd1e5b54996ed5ee5a1c992093b1f629c788
SalkovN17 <nikita_8.kz@mail.ru>
2021-09-22 21:42:09 +0700

    Добавили итоговое тестирование библиотеки

--- test/test.c
+++ test/test.c
@@ -42,7 +42,7 @@ int test1_ont__get_card(
     struct Ont_connection ont_connection[NUM_OF_RECORDS];
     memset(ont_connection, 0, sizeof(struct Ont_connection) * NUM_OF_RECORDS);
 
-    errors = ont__get_card(num_port, num_ont, ont_connection);
+    errors = ont__get_card(num_port, num_ont, ont_connection, NUM_OF_RECORDS);
 
     return errors;
 }
@@ -51,7 +51,7 @@ int test2_ont__get_card()
 {
     enum Errors errors = NO_ERRORS;
 
-    errors = ont__get_card(0, 0, NULL);
+    errors = ont__get_card(0, 0, NULL, NUM_OF_RECORDS);
 
     return errors;
 }
@@ -75,7 +75,7 @@ int test3_ont__get_card(
 
     struct Ont_connection ont_connection[NUM_OF_RECORDS];
     memset(ont_connection, 0, sizeof(struct Ont_connection) * NUM_OF_RECORDS);
-    errors = ont__get_card(num_port, num_ont, ont_connection);
+    errors = ont__get_card(num_port, num_ont, ont_connection, NUM_OF_RECORDS);
     if (0 > errors )
     {
         goto finally;
@@ -126,10 +126,154 @@ int test_ont__get_card_filter(
     return errors;
 }
 
+void print_ont_info(struct Ont_info *const card)
+{
+    printf("Serial: %s \n", card->serial);
+    printf("Eq_id: %s \n", card->eq_id);
+    printf("Fw_version: %s \n", card->fw_version);
+    printf("Link-up: %ld \n", card->link_up);
+    printf("Link-down: %ld \n", card->link_down);
+    switch (card->status)
+    {
+        case 0:
+        {
+            printf("Status: Activation \n");
+            break;
+        }
+        case 1:
+        {
+            printf("Status: Working \n");
+            break;
+        }
+        case 2:
+        {
+            printf("Status: Cfgfail \n");
+            break;
+        }
+        case 3:
+        {
+            printf("Status: Block \n");
+            break;
+        }
+        default:
+        break;
+    }
+    printf("\n");
+}
+
+void print_ont_connection(struct Ont_connection *const card)
+{
+    printf("Serial: %s \n", card->serial);
+    printf("Eq_id: %s \n", card->eq_id);
+    printf("Fw_version: %s \n", card->fw_version);
+    printf("Link-up: %ld \n", card->link_up);
+    printf("Link-down: %ld \n", card->link_down);
+    switch (card->status)
+    {
+        case 0:
+        {
+            printf("Status: Activation \n");
+            break;
+        }
+        case 1:
+        {
+            printf("Status: Working \n");
+            break;
+        }
+        case 2:
+        {
+            printf("Status: Cfgfail \n");
+            break;
+        }
+        case 3:
+        {
+            printf("Status: Block \n");
+            break;
+        }
+        default:
+        break;
+    }
+    printf("\n");
+    
+}
+
+bool compare_cards(
+    struct Ont_connection *const sended_cards,
+    struct Ont_connection *const received_cards,
+    int const num_records)
+    {
+        bool result = true;
+        for(size_t i = 0; i < NUM_OF_RECORDS; i++)
+        {
+            if(sended_cards[num_records - NUM_OF_RECORDS + i].status != received_cards[(num_records + i) % NUM_OF_RECORDS].status)
+            {
+                result = false;
+                goto finally;
+            }
+        }
+ finally:
+        return result;
+    }
+
+int test_real_work(int const num_records)
+{
+    srand(time(NULL));
+    struct Ont_info ont_info = {0};
+    struct Ont_connection received_cards[NUM_OF_RECORDS];
+    struct Ont_connection sended_cards[num_records];
+    enum Errors errors = NO_ERRORS;
+    bool compare_result = true;
+
+    ont_info.num_port = 0;
+    ont_info.num_ont = 0;
+
+    strncpy(ont_info.serial, "serial0", ONT_SERIAL_SIZE);
+    strncpy(ont_info.eq_id, "eq_id0", ONT_EQ_ID_SIZE);
+    strncpy(ont_info.fw_version, "fw_version0", ONT_FW_VERSION_SIZE);
+
+    printf("Отправили 15 карточек на порт 0 и онт 0 \n \n");
+
+    for(size_t i = 0; i < num_records; i++)
+    {
+        printf("Запись %li \n", i + 1);
+         ont_info.link_up = i;
+         ont_info.status = rand() % 4;
+         print_ont_info(&ont_info);
+         sended_cards[i].status = ont_info.status;
+         errors = ont__add_card(&ont_info);
+         if(errors < 0)
+         {
+             goto finally;
+         }
+    }
+
+    printf("Получили 10 последних карточек \n \n");
+
+    errors = ont__get_card(0, 0, received_cards, 10);
+    if(errors < 0)
+    {
+        goto finally;
+    }
+
+    for(size_t i = 0; i < NUM_OF_RECORDS; i++)
+    {
+        printf("Запись %li \n", i + 1);
+        print_ont_connection(&(received_cards[i]));
+    }
+
+    compare_result = compare_cards(sended_cards, received_cards, num_records);
+
+    compare_result ? printf("Перезапись произошла без ошибок! \n") : printf("Ошибочная перезапись! \n");
+    printf("\n");
+
+ finally:
+    return errors;
+}
+
 
 int main ()
 {
-    plan(12);
+    plan(13);
 
     cmp_ok(test1_ont__add_card(NULL), "==", NULL_PTR_ERROR,
         "Проверка функции ont__add_card при передаче в нее NULL.");
@@ -156,7 +300,11 @@ int main ()
     cmp_ok(test_ont__get_card_filter(1, 1, 1, 2, ACTIVATION, ACTIVATION), "==", NOT_FOUND_ERROR,
         "Проверка функции ont__get_card_filter при разных событиях ID.");
     cmp_ok(test_ont__get_card_filter(1, 1, 1, 1, ACTIVATION, ACTIVATION), "==", NO_ERRORS,
-        "Проверка функции ont__get_card_filter на валидность записи.");
+        "Проверка функции ont__get_card_filter на валидность записи. \n");
+
+
+    cmp_ok(test_real_work(15), "==", NO_ERRORS,
+        "Тест работоспособности библиотеки. \n");
 
     done_testing();
 }
