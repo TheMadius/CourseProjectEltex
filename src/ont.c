5ec5e6fe17811c10a8f66357b371bea1b49ffd59
TheMadius <55890893+TheMadius@users.noreply.github.com>
2021-09-22 15:43:08 +0700

    Update ont.c

--- src/ont.c
+++ src/ont.c
@@ -61,6 +61,7 @@ static void* find(
 __attribute__((constructor))
 static void _init(void) 
 {
+    pthread_rwlock_init(&lock, NULL);
     for(int i = 0; i < NUM_OF_ONT_CONNECTIONS; i++) 
     {
         list__init(&ont_records[i]);
